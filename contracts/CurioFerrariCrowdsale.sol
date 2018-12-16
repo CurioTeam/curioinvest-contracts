392cc257e3cb729bbf7a078d398be18e1bdde015
Vladimir Sushkov <volodya.sushkov@gmail.com>
2018-12-16 23:28:52 +0300

    Add Refundable part

--- contracts/CurioFerrariCrowdsale.sol
+++ contracts/CurioFerrariCrowdsale.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "./crowdsale/validation/WhitelistCrowdsale.sol";
 import "./crowdsale/validation/PausableCrowdsale.sol";
-import "./crowdsale/validation/TimedCrowdsale.sol";
+import "./crowdsale/distribution/RefundableCrowdsale.sol";
 
 /**
 * @title CurioFerrariCrowdsale
@@ -13,16 +13,18 @@ import "./crowdsale/validation/TimedCrowdsale.sol";
 // TODO: add received tokens and its rates
 // TODO: timed
 // TODO: refundable. add custom FinalizableCrowdsale (when goal reached)
-contract CurioFerrariCrowdsale is WhitelistCrowdsale, PausableCrowdsale, TimedCrowdsale {
+contract CurioFerrariCrowdsale is WhitelistCrowdsale, PausableCrowdsale, RefundableCrowdsale {
   constructor (
     uint256 openingTime,
     uint256 closingTime,
     address wallet,
-    IERC20 token
+    IERC20 token,
+    uint256 saleGoal
   )
     public
     Crowdsale(wallet, token)
     TimedCrowdsale(openingTime, closingTime)
+    RefundableCrowdsale(saleGoal)
   {
 
   }
--- contracts/crowdsale/Crowdsale.sol
+++ contracts/crowdsale/Crowdsale.sol
@@ -22,6 +22,9 @@ contract Crowdsale is Roles, ReentrancyGuard {
   // Address where funds are collected
   address private _wallet;
 
+  // Amount of tokens sold
+  uint256 private _tokensSold;
+
   constructor (address wallet, IERC20 token) public {
     require(wallet != address(0));
     require(token != address(0));
@@ -36,6 +39,20 @@ contract Crowdsale is Roles, ReentrancyGuard {
   // -----------------------------------------
   function () external payable {
     revert();
+  }
+
+  /**
+    * @return the address where funds are collected.
+    */
+  function wallet() public view returns (address) {
+    return _wallet;
+  }
+
+  /**
+    * @return the amount of tokens sold.
+    */
+  function tokensSold() public view returns (uint256) {
+    return _tokensSold;
   }
 
   /**
@@ -56,20 +73,74 @@ contract Crowdsale is Roles, ReentrancyGuard {
     buyToBeneficiary(tokenAddress, amount, msg.sender);
   }
 
-
   function buyToBeneficiary(IERC20 tokenAddress, uint256 amount, address beneficiary) public {
     _preValidatePurchase(tokenAddress, amount, beneficiary);
 
-    //...
+    // TODO: add exchange logic (!!!)
+
+
+    uint256 foreignTokenAmount = amount;
+
+    require(tokenAddress.transferFrom(msg.sender, address(this), foreignTokenAmount));
+
+    // Available tokens
+    uint256 tokenBalance = _token.balanceOf(address(this));
+    require(tokenBalance > 0);
+
+    // How many tokens will be buy
+    // uint256 tokens = _getTokenAmount(tokenAddress, foreignTokenAmount); // TODO: add this function
+    uint256 tokens = foreignTokenAmount; // TODO: remove 1:1 rate
+
+    if (tokens > tokenBalance) {
+      tokens = tokenBalance;
+      // foreignTokenAmount = _inverseGetTokenAmount(tokenAddress, tokens); // TODO: add this function
+      foreignTokenAmount = tokens; // TODO: remove 1:1 rate
+
+      uint256 senderForeignTokenExcess = amount.sub(foreignTokenAmount);
+      tokenAddress.transfer(beneficiary, senderForeignTokenExcess); // Excess to beneficiary TODO: check logic (!!!)
+      /*
+      // TODO: add event
+      emit SendTokensExcess(
+        beneficiary,
+        senderForeignTokenExcess
+      );
+      */
+    }
+
+    // update state
+    _tokensSold = _tokensSold.add(tokens);
+
+    // receivedTokens[_tokenAddress].raised = receivedTokens[_tokenAddress].raised.add(foreignTokenAmount);
+
+    _processPurchase(beneficiary, tokens); // send tokens to beneficiary
+    /*
+    // TODO: add event
+    emit TokenForTokenPurchase(
+      _sender,
+      _sender,
+      foreignTokenAmount,
+      tokens
+    );
+    */
+
+    _updatePurchasingState(tokenAddress, tokens, beneficiary);
+
+    /*
+    // TODO: remove if necessary
+
+    _forwardFunds();
+    _postValidatePurchase(beneficiary, weiAmount);
+    */
   }
 
   /**
    * @dev Withdraws any tokens from this contract to wallet.
    * @param tokenContract The address of the foreign token
    */
-  function withdrawTokens(IERC20 tokenContract) onlyOwner external {
+  function withdrawForeignTokens(IERC20 tokenContract) onlyOwnerOrAdmin external {
     // TODO: disallow withdraw stable tokens (freeze)
     // ...
+    // TODO: or remove this function
   }
 
   // -----------------------------------------
@@ -78,13 +149,32 @@ contract Crowdsale is Roles, ReentrancyGuard {
 
   function _preValidatePurchase(IERC20 tokenAddress, uint256 amount, address beneficiary) internal view {
     require(tokenAddress != address(0));
+    // require(receivedTokens[_tokenAddress].rate > 0); TODO: add functionality
+
     require(beneficiary != address(0));
     require(amount > 0);
   }
 
+  /**
+  * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+  * @param beneficiary Address receiving the tokens
+  * @param amount Number of tokens to be purchased
+  */
+  function _processPurchase(address beneficiary, uint256 amount) internal {
+    _deliverTokens(beneficiary, amount);
+  }
+
+  /**
+   * @dev Source of tokens.
+   * @param beneficiary Address performing the token purchase
+   * @param amount Number of tokens to be emitted
+   */
+  function _deliverTokens(address beneficiary, uint256 amount) internal {
+    _token.transfer(beneficiary, amount);
+  }
+
   function _updatePurchasingState(IERC20 tokenAddress, uint256 amount, address beneficiary) internal {
-    // ...
-    // TODO: save deposits (for beneficiary)
+
   }
 
 }
